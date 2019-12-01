99fa5b252db1c1c37307a6b3af0c0fd08dc5a8ce
zadira <zadirakh@gmail.com>
2019-12-01 15:19:07 +0200

    component's connection

--- vue/src/Example.vue
+++ vue/src/Example.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h1>Example text</h1>
-        <app-input></app-input>
+        <h1>{{ message }}</h1>
+        <app-input :msg="message"></app-input>
     </div>
 </template>
 
@@ -9,6 +9,11 @@
 import Input from './Field.vue';
 
 export default {
+    data(){
+        return {
+            message: 'Exampled Text'
+        }
+    },
     components: {
         'app-input': Input
     }
--- vue/src/Field.vue
+++ vue/src/Field.vue
@@ -7,6 +7,7 @@
 
 <script>
 export default {
+    props:['msg'],
     data () {
         return {
             mess: ''
