ed6cfea5eba1a4969a40368ab00a167991c22462
zadira <zadirakh@gmail.com>
2019-12-01 14:54:19 +0200

    creating local component

--- vue/src/Example.vue
+++ vue/src/Example.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>Example text</h1>
+        <app-input></app-input>
     </div>
 </template>
+
+<script>
+import Input from './Field.vue';
+
+export default {
+    components: {
+        'app-input': Input
+    }
+}
+</script>
